27b42a508372f7f7497f0e759bc4a7f8029f1384
Archak Nath <112867859+archakNath@users.noreply.github.com>
2026-04-30 00:43:23 +0530

    prism changes

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: [], // Make sure prismjs is not externalized
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
-  },
-  optimizeDeps: {
-    include: ['prismjs', '@lexical/code'],
-  },
+  plugins: [react(), tailwindcss(),],
 })
