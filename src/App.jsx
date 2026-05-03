733977fe2aa0c81a40c0fb2ad0fb4664bca98a97
Archak Nath <112867859+archakNath@users.noreply.github.com>
2026-05-03 21:24:05 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import Landing from './pages/Landing'
 import Blogs from './pages/Blogs'
-import BlogAdmin from './pages/BlogAdmin'
 import BlogPage from './pages/BlogPage'
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
     <Routes>
       <Route index element={<Landing />} />
       <Route path='/blog' element={<Blogs />} />
-      <Route path='/admin' element={<BlogAdmin />} />
       <Route path="/blog/:slug" element={<BlogPage />} />
     </Routes>
   )
