b040a3c760c3520f9ce1a9e1610026d831ef6067
Archak Nath <112867859+archakNath@users.noreply.github.com>
2026-05-03 21:29:28 +0530

    commit admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import Landing from './pages/Landing'
 import Blogs from './pages/Blogs'
+import BlogAdmin from './pages/BlogAdmin'
 import BlogPage from './pages/BlogPage'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route index element={<Landing />} />
       <Route path='/blog' element={<Blogs />} />
+      <Route path='/admin' element={<BlogAdmin />} />
       <Route path="/blog/:slug" element={<BlogPage />} />
     </Routes>
   )
